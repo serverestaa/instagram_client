5c304edda3f4b266e00c0dfb7eaaef3604c4c7b0
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-25 17:02:35 +0500

    upload photos and oauth2

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 from db.database import engine
 from db import models
 from routers import user, post
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(post.router)
 models.Base.metadata.create_all(engine)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
